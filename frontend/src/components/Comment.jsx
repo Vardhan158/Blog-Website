93c3ead9180cc4886ac88bbd862ae7fd7ee0884b
Vardhan158 <harshavardhandevang@gmail.com>
2025-10-22 13:00:13 +0530

    Update Comment.jsx

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -16,8 +16,8 @@ const Comment = ({ blogId }) => {
 
       try {
         const url = blogId
-          ? `http://localhost:5000/api/comments/${blogId}` 
-          : "http://localhost:5000/api/comments"; 
+          ? `https://blog-website-1oq8.onrender.com/api/comments/${blogId}` 
+          : "https://blog-website-1oq8.onrender.com/api/comments"; 
 
         const res = await axios.get(url, {
           headers: token ? { Authorization: `Bearer ${token}` } : {},
